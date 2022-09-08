89377fb5977597cf428a6b3294c91d3ca96de9aa
Burmatov.VO <vladimir.burm1984@gmail.com>
2022-09-08 09:52:01 +0700

    Add files

---
+++ Test.py
@@ -0,0 +1,48 @@
+
+# "А роза #упала - на лапу Азора   ?!"
+
+my_text = input("Введите текст - ")
+my_tuple = (".",","," ","#","-","?","!","*")
+global rezult
+
+def reverse(rev):
+    return rev[::-1]
+
+def func (pol):
+    global rezult
+    for i in my_tuple:
+        if i in pol:
+            rezult=(pol.replace(i,""))
+            func(rezult)
+
+text = my_text.lower()
+
+if __name__ == '__main__':
+    func(text)
+
+print("Да, это палиндром" if reverse(rezult) == rezult else "Нет,это не палиндром")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
